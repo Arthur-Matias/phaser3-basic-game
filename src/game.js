fc48f7f83642d48c8f80df348438a2c2116e1b2b
Arthur Matias <57842765+Arthur-Matias@users.noreply.github.com>
2021-05-30 20:01:35 -0300

    Update game.js

--- src/game.js
+++ src/game.js
@@ -27,12 +27,12 @@ const game = new Phaser.Game(config);
 function preload ()
 {
     // 800 x 600
-    this.load.image('sky', '../assets/sky.png');
+    this.load.image('sky', 'https://github.com/Arthur-Matias/phaser3-basic-game/blob/main/assets/sky.png?raw=true');
     // 400 x 32
-    this.load.image('ground', '../assets/platform.png');
-    this.load.image('star', '../assets/star.png');
-    this.load.image('bomb', '../assets/bomb.png');
-    this.load.spritesheet('dude', '../assets/dude.png', {frameWidth: 32, frameHeight: 48})
+    this.load.image('ground', 'https://github.com/Arthur-Matias/phaser3-basic-game/blob/main/assets/assets/platform.png?raw=true');
+    this.load.image('star', 'https://github.com/Arthur-Matias/phaser3-basic-game/blob/main/assets/assets/star.png?raw=true');
+    this.load.image('bomb', 'https://github.com/Arthur-Matias/phaser3-basic-game/blob/main/assets/bomb.png?raw=true');
+    this.load.spritesheet('dude', 'https://github.com/Arthur-Matias/phaser3-basic-game/blob/main/assets/dude.png?raw=true', {frameWidth: 32, frameHeight: 48})
 }
 
 function create ()
