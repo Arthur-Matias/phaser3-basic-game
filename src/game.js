e0a87dc4f51aaae55ef91cce4e371828a69548aa
Arthur Matias <57842765+Arthur-Matias@users.noreply.github.com>
2022-12-29 13:54:09 -0300

    Update game.js

--- src/game.js
+++ src/game.js
@@ -27,12 +27,12 @@ const game = new Phaser.Game(config);
 function preload ()
 {
     // 800 x 600
-    this.load.image('sky', '/assets/sky.png');
+    this.load.image('sky', '../assets/sky.png');
     // 400 x 32
-    this.load.image('ground', '/assets/.png');
-    this.load.image('star', '/assets/star.png');
-    this.load.image('bomb', '/assets/bomb.png');
-    this.load.spritesheet('dude', '/assets/dude.png', {frameWidth: 32, frameHeight: 48})
+    this.load.image('ground', '../assets/.png');
+    this.load.image('star', '../assets/star.png');
+    this.load.image('bomb', '../assets/bomb.png');
+    this.load.spritesheet('dude', '../assets/dude.png', {frameWidth: 32, frameHeight: 48})
 }
 
 function create ()
